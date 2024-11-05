29fa97eb0d6f341c27eb2214fd1b1191dc04a7f1
BorrachaFox <daviantoniosantos@outlook.com>
2024-11-05 14:10:43 -0300

    feat(config): add MySQL database connection config

---
+++ src/config/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const pool = mysql.createPool({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+const closePool = async () => {
+  await pool.end();
+};
+
+module.exports = { pool, closePool };

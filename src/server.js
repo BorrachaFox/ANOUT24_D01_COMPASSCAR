dc770caf4cfa3310135ccca359ab50db2237767f
BorrachaFox <daviantoniosantos@outlook.com>
2024-11-05 14:15:20 -0300

    fix(server): close db connection pool on server shutdown

--- src/server.js
+++ src/server.js
@@ -1,9 +1,21 @@
 require('dotenv').config();
 const app = require('./app');
+const { closePool } = require('./config/db');
 
 const SERVER_PORT = process.env.SERVER_PORT || 3001;
 
 app.listen(SERVER_PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server is running: http://localhost:${SERVER_PORT}`);
+});
+
+// Close pool connection
+process.on('SIGINT', async () => {
+  await closePool();
+  process.exit(0);
+});
+
+process.on('SIGTERM', async () => {
+  await closePool();
+  process.exit(0);
 });

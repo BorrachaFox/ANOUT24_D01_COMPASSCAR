90cb025c6da4de6f7fb31a89dc071237a4d4f109
BorrachaFox <daviantoniosantos@outlook.com>
2024-11-08 12:30:49 -0300

    feat(carsRouter): add route for updating car items

--- src/routes/carsRouter.js
+++ src/routes/carsRouter.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const {
   createCarController,
+  addCarItemsController,
 } = require('../controllers/carController');
 
 const router = Router();
 
 router.post('/', createCarController);
+router.put('/:id/items', addCarItemsController);
 
 module.exports = router;

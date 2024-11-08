20c0cbd3893bd44068d98c3d71a13b6610e4b34a
BorrachaFox <daviantoniosantos@outlook.com>
2024-11-08 12:30:20 -0300

    feat(carController): add addCarItemsController for adding items to a car

--- src/controllers/carController.js
+++ src/controllers/carController.js
@@ -3,7 +3,16 @@ const { CarService } = require('../services/carService');
 const createCarController = async (req, res, next) => {
   try {
     const result = await CarService.create(req.body);
-    res.status(201).json(result[0]);
+    res.status(201).json(result);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const addCarItemsController = async (req, res, next) => {
+  try {
+    await CarService.addItem(req.params.id, req.body);
+    res.status(204).send();
   } catch (err) {
     next(err);
   }
@@ -11,4 +20,5 @@ const createCarController = async (req, res, next) => {
 
 module.exports = {
   createCarController,
+  addCarItemsController,
 };

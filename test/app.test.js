498f6b6c14451ca1734fb3024119601b6db5a8dd
BorrachaFox <daviantoniosantos@outlook.com>
2024-11-08 09:36:38 -0300

    chore(tests): translate test descriptions in app.test file from Portuguese to English

--- test/app.test.js
+++ test/app.test.js
@@ -3,7 +3,7 @@ const request = require('supertest');
 
 const app = require('../src/app');
 
-test('Deve responder na raiz', () => {
+test('Should return status 200 on root route', () => {
   return request(app).get('/')
     .then((res) => {
       expect(res.status).toBe(200);

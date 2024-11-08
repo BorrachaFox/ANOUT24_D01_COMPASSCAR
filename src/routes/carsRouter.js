c14bff3e72f46ef821288addce1001ef19ffd2cf
BorrachaFox <daviantoniosantos@outlook.com>
2024-11-08 13:18:13 -0300

    feat(carsRouter): add route for retrieving car details by ID

--- src/routes/carsRouter.js
+++ src/routes/carsRouter.js
@@ -2,11 +2,13 @@ const { Router } = require('express');
 const {
   createCarController,
   addCarItemsController,
+  getCarController,
 } = require('../controllers/carController');
 
 const router = Router();
 
 router.post('/', createCarController);
+router.get('/:id', getCarController);
 router.put('/:id/items', addCarItemsController);
 
 module.exports = router;
